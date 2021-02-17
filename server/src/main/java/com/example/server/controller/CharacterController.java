1946e7f73b632b8d145784892df2a53e3e69eaea
Giana Mylan <Giana Mylan>
2021-02-17 13:17:53 -0500

    fixed PSQL issue, new error with autowired and bean

--- server/src/main/java/com/example/server/controller/CharacterController.java
+++ server/src/main/java/com/example/server/controller/CharacterController.java
@@ -2,6 +2,7 @@ package com.example.server.controller;
 import com.example.server.model.Characters;
 import com.example.server.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,5 +22,15 @@ public class CharacterController {
     @PostMapping
     public Characters createCharacter(@RequestBody Characters characters) {
         return characterService.createCharacter(characters);
+    }
+
+    @PatchMapping
+    public Characters updateCharacter(@RequestBody Characters characters) {
+        return characterService.updateCharacter(characters);
+    }
+
+    @DeleteMapping
+    public HttpStatus deleteCharacter(@RequestParam Long id) {
+        return characterService.deleteCharacter(id);
     }
 }
