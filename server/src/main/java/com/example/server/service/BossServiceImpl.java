9439956b28f8cc1c8c831d5fea382300c7c463d6
Giana Mylan <Giana Mylan>
2021-02-18 15:12:22 -0500

    boss service impl create/update/delete

--- server/src/main/java/com/example/server/service/BossServiceImpl.java
+++ server/src/main/java/com/example/server/service/BossServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.server.service;
 import com.example.server.model.Bosses;
 import com.example.server.repository.BossRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class BossServiceImpl {
@@ -13,12 +15,23 @@ public class BossServiceImpl {
 
     @Override
     public Iterable<Bosses> getBosses() {
-        
+        return bossRepository.findAll();
     }
 
     @Override
-    public Bosses createBoss() {
-        return createBoss();
+    public Bosses createBoss(Bosses bosses) {
+        return bossRepository.save(bosses);
+    }
+
+    @Override
+    Bosses updateBoss(Bosses bosses) {
+        return bossRepository.save(bosses);
+    }
+
+    @Override
+    public HttpStatus deleteBoss(Long id) {
+        bossRepository.deleteById(id);
+        return HttpStatus.OK;
     }
 
 }
