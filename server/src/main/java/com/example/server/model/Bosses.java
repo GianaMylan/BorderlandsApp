2b1f2cfdf413955f18eef418d0d89201733e3ae8
Giana Mylan <Giana Mylan>
2021-02-18 14:49:12 -0500

    bosses model added

---
+++ server/src/main/java/com/example/server/model/Bosses.java
@@ -0,0 +1,67 @@
+package com.example.server.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bl3_bosses")
+public class Bosses {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String race;
+    @Column
+    private String gender;
+    @Column
+    private String affiliation;
+    @Column
+    private Boolean dedicated_loot_drop;
+
+    public Bosses() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public Boolean getDedicated_loot_drop() {
+        return dedicated_loot_drop;
+    }
+
+    public void setDedicated_loot_drop(Boolean dedicated_loot_drop) {
+        this.dedicated_loot_drop = dedicated_loot_drop;
+    }
+}
