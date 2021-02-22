573e953fef88fb333f8e6d378ae0654d25426b5a
Giana Mylan <Giana Mylan>
2021-02-22 10:40:09 -0500

    fixed delete on bosses

--- server/src/main/java/com/example/server/controller/BossController.java
+++ server/src/main/java/com/example/server/controller/BossController.java
@@ -33,8 +33,8 @@ public class BossController {
         return bossService.updateBoss(bosses);
     }
 
-    @DeleteMapping("/{id}")
-    public HttpStatus deleteBoss(@RequestParam Long boss_id) {
+    @DeleteMapping("/{boss_id}")
+    public HttpStatus deleteBoss(@PathVariable Long boss_id) {
         return bossService.deleteBoss(boss_id);
     }
 }
